464618af9c9c882c2f9250d01a02994ab6a84cbf
Karan Pal <karanpal03040@gmail.com>
2024-07-21 14:15:39 +0530

    Created a funtion which reverse a string through Recurion

---
+++ Stack/reverseString.js
@@ -0,0 +1,35 @@
+let data = []
+let currentSize = data.length
+
+function push(newValue) {
+    data[currentSize] = newValue
+    currentSize += 1;
+}
+
+function pop() {
+    lastRemovedItem = data[currentSize-1]
+    currentSize -= 1;
+    data.length = currentSize
+    return lastRemovedItem
+}
+
+function reverseString(item) {
+
+    // console.log(item)
+
+    for (let i = 0; i < item.length; i++) {
+        push(item[i])
+    }
+    for (let i = 0; i < item.length; i++) {
+        item[i] = pop()
+        // pop()
+    }
+
+}
+
+let str = 'karan';
+str = str.split('');
+
+reverseString(str)
+
+console.log(str)
